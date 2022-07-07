8a9cd308abb75cef4460531216b7da830aaf9d14
klibin <km.putnam2@gmail.com>
2022-07-07 16:22:56 -0600

    updated routes for landing page

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ function App() {
           <div className="container">
             <Navbar />
             <Routes>
-              {/* <Route path="/" element={user ? <Dashboard /> : <Navigate to="/login" />} /> */}
-              <Route path="/" element={user ? <Dashboard /> : <Landing />} />
+              <Route path="/" element= {user? <Navigate to="/dashboard" /> : <Landing />} />
+              <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
               <Route path="/create" element={user ? <Create /> : <Navigate to="/login" />} />
               <Route path="/projects/:id" element={user ? <Project /> : <Navigate to="/login" />} />
               <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
